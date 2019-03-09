242b9e1d83e96f5e86459f20d53aae5c7706ac2e
Nazar Pasternak <43440994+heeveG@users.noreply.github.com>
2019-03-09 21:14:21 +0200

    Update spoonacularAPI.py

--- API_examples/spoonacularAPI.py
+++ API_examples/spoonacularAPI.py
@@ -15,19 +15,10 @@ def find_recipes(ingredients):
 
     connection = requests.get(
         "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=3&ranking=1&ingredients={}".format(
-             ing),
+            ing),
         headers={"X-RapidAPI-Key": "key"})
-    data_dict = dict()
     data = connection.json()
-    for i in data:
-        meal = requests.get(
-            "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{}/information".format(i['id']),
-            headers={"X-RapidAPI-Key": "key"})
-        data_dict[i['id']] = meal.json()
-
-    with open("data.json", "w") as file:
-        json.dump(data_dict, file, indent=4, separators=(',', ': '))
-    return None
+    print(data)
 
 
 if __name__ == "__main__":
