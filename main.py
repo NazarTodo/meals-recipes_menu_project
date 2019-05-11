9ffb416aa508e99c4cae162f52d35dbe38f93feb
Nazar Pasternak <43440994+heeveG@users.noreply.github.com>
2019-05-11 19:19:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from API_functions.spoonacular_api import find_recipes
 from Menu_ADT.menu_adt import Menu
 from functions.product_price import price_product
 
-ing = [input() for i in range(3)]
+ing = [input("Enter an ingredient:\n") for i in range(3)]
 find_recipes(ing)
-main_dish = Menu('data.json')
+main_dish = Menu('API_functions/json_files/data.json')
 products = main_dish.get_products()
 prices = dict()
 for key, value in products.items():
