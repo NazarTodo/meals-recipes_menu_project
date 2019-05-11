7983dea85c6cf6207d2f41186a90e4729cd43daf
Nazar Pasternak <43440994+heeveG@users.noreply.github.com>
2019-05-11 18:04:01 +0300

    Create menu_adt.py

---
+++ Menu ADT/menu_adt.py
@@ -0,0 +1,70 @@
+import json
+from linkedlist import LinkedList
+
+
+class Menu:
+    """
+    ADT for food menu
+    """
+
+    def __init__(self, meals_file):
+        """
+        :param meals_file: json file
+        """
+        self.meals_file = meals_file
+
+        with open(self.meals_file) as f:
+            self._data = json.load(f)
+
+        self._products = dict()
+        self._instructions = dict()
+        self._images = dict()
+
+        # Call methods to fill dictionaries
+        self.__find_products()
+        self.__find_instructions()
+        self.__find_image()
+
+    def __find_products(self):
+        """
+        Fills _products dict with ingredients for each meal
+        """
+        for i in self._data.values():
+            self._products[i['title']] = LinkedList()
+            for j in i["extendedIngredients"]:
+                self._products[i['title']].add(
+                    (j['name'], j['measures']['metric']['amount'], j['measures']['metric']['unitShort']))
+            self._products[i['title']].change_metric()
+
+    def __find_instructions(self):
+        """
+        Fills _instructions dict with meal as a key and meal's preparing instructions as a value
+        """
+        for i in self._data.values():
+            self._instructions[i['title']] = [i["instructions"]]
+            self._instructions[i['title']].append([i["pricePerServing"]])
+
+    def __find_image(self):
+        """
+        Fills _images dict with meal as a key and its image as a value
+        """
+        for i in self._data.values():
+            self._images[i['title']] = i['image']
+
+    def get_products(self):
+        """
+        :return: _products dict
+        """
+        return self._products
+
+    def get_instructions(self):
+        """
+        :return: _instructions dict
+        """
+        return self._instructions
+
+    def get_image(self):
+        """
+        :return: _image dict
+        """
+        return self._images
