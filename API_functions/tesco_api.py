95590e8312723ed6a431da5aba669dcb7b945c1e
Nazar Pasternak <43440994+heeveG@users.noreply.github.com>
2019-05-11 18:52:03 +0300

    Update tesco_api.py

--- API_functions/tesco_api.py
+++ API_functions/tesco_api.py
@@ -11,7 +11,7 @@ def find_price(ing):
         conn = requests.get("https://dev.tescolabs.com/grocery/products/?query={}&offset=0&limit=10".format(ing),
                             headers={'Ocp-Apim-Subscription-Key': 'c5683c24867d481dba0a91e4025d03a0'})
         data = conn.json()
-        with open("products.json", "w") as file:
+        with open("json_files/products.json", "w") as file:
             json.dump(data, file, indent=4, separators=(',', ': '))
     except Exception as e:
         print("[Errno {0}] {1}".format(e.errno, e.strerror))
