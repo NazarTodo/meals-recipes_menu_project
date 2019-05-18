6e239f1a29ca6cd70e87e27689a408410915462e
Nazar Pasternak <43440994+heeveG@users.noreply.github.com>
2019-05-18 15:05:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ from API_functions.spoonacular_api import find_recipes
 from Menu_ADT.menu_adt import Menu
 from functions.product_price import price_product
 
-ing = [input("Enter an ingredient:\n") for i in range(3)]
-find_recipes(ing)
-main_dish = Menu('../API_functions/json_files/data.json')
-products = main_dish.get_products()
-prices = dict()
-for key, value in products.items():
-    price = []
-    current = value._head
-    while current is not None:
-        price.append(price_product(current))
-        current = current.next
-    price = sum(price)
-    prices[key] = round(price, 2)
+
+def main(ing):
+    find_recipes(ing)
+    main_dish = Menu('../API_functions/json_files/data.json')
+    products = main_dish.get_products()
+    images = main_dish.get_image()
+    recipes = main_dish.get_instructions()
+    prices = dict()
+    result = {}
+    for key, value in products.items():
+
+        price = []
+        current = value._head
+        while current is not None:
+            price.append(price_product(current))
+            current = current.next
+        price = sum(price)
+        prices[key] = round(price, 2)
+    for key in prices:
+        result[key] = prices[key], images[key], recipes[key]
+    return result
+
 
 if __name__ == '__main__':
-    print(prices)
-
+    print(main(['pork', 'potato', 'fries']))
